da58a008a3967f312035f8e95498da2a7372d619
Tomasz Sikora <sikora507@hotmail.com>
2017-04-21 11:56:20 +0200

    introduced navbar component

---
+++ DistributedClients/src/Navbar.vue
@@ -0,0 +1,32 @@
+﻿<template>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">{{name}}</a>
+            </div>
+
+            <form class="navbar-form navbar-left">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div><!-- /.container-fluid -->
+    </nav>
+</template>
+
+<script lang="ts">
+    export default {
+        data() {
+            return {
+                greeting: 'Hello'
+            }
+        },
+        props: ['name']
+    }
+</script>
+
+<style>
+
+</style>
